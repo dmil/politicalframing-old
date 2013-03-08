81852c629fa8b0b2bfdabd53c0441ac15ea0d865
Dhrumil <dhrumilmehta2013@u.northwestern.edu>
2013-03-08 17:07:00 -0600

    Show Berkman Simple / Clean Code
    Excluded extended frames - added some comments.

--- framemaker.py
+++ framemaker.py
@@ -1,25 +1,109 @@
+"""This module allows the user to build frames from the python shell.
+
+Vocabulary:
+Frame - a set of wordnet.synset objects that are collectivey related to a particular object
+
+Functions:
+pickleframe(word)
+picklemultiframe(framewords,word)
+addset2pickledframe(frame, myset)
+load(word)
+makeframe(frameword)
+
+"""
+
 from nltk.corpus import wordnet
 from sets import Set
 
 import wordnettools
-import testinggod
+import frameextender
 import pickle
 
-frameword = ''
+def makeframe(frameword):
+    """Returns a frame (Set of synsets) based on frame word and user input to shell.
+        See module docstring for more info.
+
+        Arguments:
+        frameword - a single word string
+
+        Note:
+        To best understand the functionality of this module, build a frame about 'love'
+        and you can see its different senses very clearly.
+        
+    """
+
+    framewordsynsets = wordnet.synsets(frameword) #synsets of frameword
+    frame = Set() # words to include in frame
+    parsedsynsets = Set() #keep track of parsed synsets so you don't ask about the same one twice
+
+    
+    for synset in framewordsynsets: #loop through each possible meaning of a word
+        if not (synset in parsedsynsets):
+            parsedsynsets.add(synset) #mark synset as "parsed"
+            
+            # Ask user if the synset is relevant to the frame they want to build
+            print '\nDoes this fit the frame you have in mind?: '
+            print str(synset) + '   ' + synset.definition
+            print synset.examples
+
+   
+            #I have chosen not to ask the user about each realted form as they are
+            #synonyms and it becomes cumbersome, but chose to display these anyways
+            #to clarify the meaning that 'synset' conveys. 
+            for rsynset in wordnettools.getrelatedforms(synset): 
+                if not(rsynset in parsedsynsets):
+                    print '\t' + str(rsynset) + '   ' + rsynset.definition
+                    print '\t' + str(rsynset.examples)
+                    parsedsynsets.add(rsynset) #mark as "parsed" 
+
+            #Request user to accept or reject frame
+            userin = raw_input('y or n: ')
+            
+            if userin.lower() == 'y':
+                #include one hypernym, all lemmas, all related forms, all hyponyms, and all domain terms of synset
+                #see implementation of "all-things" in wordnettools
+                for subsynset in (wordnettools.gethypernymsrecursive(synset)
+                             + wordnettools.getlemmas(synset)
+                             + wordnettools.getrelatedforms(synset)
+                             + wordnettools.gethyponymsrecursive(synset)
+                             + wordnettools.getdomainterms(synset)):
+                    frame.add(wordnettools.getnamepretty(subsynset))
+                                    
+            elif userin.lower() == 'n':
+                print str(synset.name) + ' not included'
+            else:
+                print 'Error - only y or n are valid responses'
+    
+
+    return frame
 
 def pickleframe(word):
+    """Make and pickle a frame about 'word', dump it in 'word.txt'."""
     fname = word + '.txt'
     f = open(fname, 'w')
     pickle.dump(makeframe(word),f)
     f.close()
 
 def picklemultiframe(framewords,word):
+    """Make and pickle a frame about multiple framewords, dump it in 'word.txt'.
+
+        Arguments:
+        word - name of the file and overall name of the frame
+        framewords - list of words which will collectively make up the frame
+
+    """
     fname = '1' + word + '.txt'
     f = open(fname, 'w')
     pickle.dump(multimakeframe(framewords,word),f)
     f.close()
 
 def addset2pickledframe(frame, myset):
+    """Add a set of synsets to an existing pickled frame and save in a .txt file.
+
+        Arguments:
+        frame - a pickled frame (Set of synsets)
+        myset - set of synsets to add to the existing frame
+    """
     fname = frame + '.txt'
     newset = pickle.load(frame).union(myset)
     f = open(fname, 'w')
@@ -28,72 +112,11 @@ def addset2pickledframe(frame, myset):
     return newset
 
 def load(word):
+    """Load a frame from 'word.txt' and return it."""
     fname = word + '.txt'
     f = open(fname, 'r')
     p = pickle.load(f)
     f.close()
     return p
-
-def makeframe(frameword):
-    framesynsets = wordnet.synsets(frameword)
-    #### which definitions would you like to include in your frame?
-    includedwords = []
-    extension = []
-    parsedsynsets = Set()
-    for synset in framesynsets:
-        if not (synset in parsedsynsets):
-            parsedsynsets.add(synset)
-            print '\does this fit the frame?: ' + str(frameword)
-            print str(synset) + '   ' + synset.definition
-            print synset.examples
-
-            for rsynset in wordnettools.getrelatedforms(synset):
-                if not(rsynset in parsedsynsets):
-                    print '\t' + str(rsynset) + '   ' + rsynset.definition
-                    print '\t' + str(rsynset.examples)            
-
-            want = True
-            correctinput = False
-            while correctinput == False:
-                userin = raw_input('y or n: ')
-                if userin == 'y':
-                    want = True
-                    correctinput = True
-                    # Related term check
-                    print '\nrelated forms\n'
-                    for rsynset in wordnettools.getrelatedforms(synset):
-                        if not(rsynset in parsedsynsets):
-                            parsedsynsets.add(rsynset)
-                            #print '\t' + str(rsynset) + '   ' + rsynset.definition
-                            #print '\t' + str(rsynset.examples)
-
-                            want2 = True
-                            correctinput2 = False
-                            while correctinput2 == False:
-                                userin = 'y'
-                                if userin == 'y':
-                                    want2 = True
-                                    correctinput2 = True
-                                elif userin == 'n':
-                                    want2 = False
-                                    correctinput2 = True
-                                else:
-                                    print '\tError - only y or n are allowed'
-                                    
-                            if want2:
-                                includedwords = includedwords + wordnettools.getallthingspretty(rsynset)
-                                extension = extension + testinggod.frameextension(rsynset)
-                        
-                elif userin == 'n':
-                    want = False
-                    correctinput = True
-                else:
-                    print 'Error - only y or n are allowed'
-        
-            if want:
-                includedwords = includedwords + wordnettools.getallthingspretty(synset)
-                extension = extension + testinggod.frameextension(synset)
-
-    return set(includedwords)
 
 
