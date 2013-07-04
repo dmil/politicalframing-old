cc25cf5600eb8be0af64293e3beb490affc437ff
dmil <dhrumil.mehta@gmail.com>
2013-07-03 23:30:58 -0400

    updated dataminer for linux and windows filepaths

--- dataminer.py
+++ dataminer.py
@@ -6,19 +6,27 @@ import os
 import math
 
 def output_files(data, phrase, page_no):
-    folder_path = "C:\\Users\\Dhrumil\\Documents\\GitHub\\PoliticalFraming\\data\\" + phrase
+    #windows
+    #folder_path = os.path.dirname(__file__) + "\\data\\" + phrase
+
+    #linux
+    folder_path = os.path.dirname(__file__) + "/data/" + phrase
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         
     count = (page_no * 1000) - 999
     for result in data[u'results']:
-        outputfile = open(folder_path + "\\\\" + str(count) + '.json', 'w')
+        #windows
+        #outputfile = open(folder_path + "\\\\" + str(count) + '.json', 'w')
+        #linux
+        outputfile = open(folder_path + "/" + str(count) + '.json', 'w')
         outputfile.write(str(result))
         outputfile.close()
         count = count + 1
 
 
-phrase = 'war' #phrase to search for
+phrase = 'gay' #phrase to search for
+#print phrase #debug
 results_per_page = 1000 #number of results per page
 page_number = 1
 query_parameters = {'apikey': '8e87cf0e8a92499e9d14b67165f7018f',
@@ -29,9 +37,10 @@ endpoint = 'http://capitolwords.org/api/text.json'
 response = requests.get(endpoint, params = query_parameters)
 data = response.json
 num_speeches_found = data[u'num_found']
+print num_speeches_found
 output_files(data,phrase, page_number) #write these files
 
-for page_no in range(int(math.ceil(float(num_speeches_found)/results_per_page))):    
+for page_no in range(int(math.ceil(float(num_speeches_found)/results_per_page))):
     page_number = page_number + 1
     query_parameters = {'apikey': '8e87cf0e8a92499e9d14b67165f7018f',
                                    'phrase': phrase,
---
+++ test.py
@@ -0,0 +1,9 @@
+#mine data with metadata using python
+import requests
+#import pprint 
+#import json
+import os 
+import math
+
+folder_path = os.path.dirname(__file__) + "/data"
+print folder_path        
