84cc085c0e5397c36de915ea5aace5215ef68692
dmil <dhrumil.mehta@gmail.com>
2013-07-04 00:58:16 -0400

    remove data

--- test.py
+++
@@ -1,9 +0,0 @@
-#mine data with metadata using python
-import requests
-#import pprint 
-#import json
-import os 
-import math
-
-folder_path = os.path.dirname(__file__) + "/data"
-print folder_path        
